7a3ce9fcc78b09253a3db96d17a5ce9a0ab716df
Gjermund K. Lie <gjermund.k.lie@gmail.com>
2018-10-15 06:52:15 +0200

    Utvidet med konsoll-app, startet med srp

--- srp/src/main/java/before/CustomerService.java
+++ srp/src/main/java/before/CustomerService.java
@@ -3,14 +3,19 @@ package before;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
 public class CustomerService {
+
+    public static final String FOLDER = "customers";
 
     public Customer createCustomer(String name, String email) {
 
@@ -31,14 +36,69 @@ public class CustomerService {
                 .build();
 
         // Persist customer
-        String fileName = name + ".data";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))){
-            writer.write(customer.toString());
-        } catch (IOException e) {
+        File folder = new File(FOLDER);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        File file = new File(folder, customer.getName() + ".data");
+        List<String> content = new ArrayList<>();
+        content.add(customer.getName());
+        content.add(customer.getEmail());
+        try {
+            Files.write(file.toPath(), content, StandardOpenOption.CREATE);
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
 
         return customer;
     }
+
+    public List<Customer> getAllCustomers() {
+        List<Customer> customers = new ArrayList<>();
+        File customerFolder =  new File(FOLDER);
+        if (!customerFolder.exists()) {
+            return customers;
+        }
+        File[] files = customerFolder.listFiles();
+        if (files == null) {
+            return customers;
+        }
+        for (File file : files) {
+            Customer customer = readFromFile(file);
+            if (customer != null) {
+                customers.add(customer);
+            }
+        }
+        return customers;
+    }
+
+    public Customer findCustomerByName(String name) {
+        return readFromFile(new File(FOLDER + File.separator + name));
+    }
+
+    public void resetStorage() {
+        File folder = new File(FOLDER);
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                file.delete();
+            }
+        }
+    }
+
+    private Customer readFromFile(File file) {
+        try {
+            List<String> content = Files.readAllLines(file.toPath());
+            return Customer.builder()
+                    .name(content.get(0))
+                    .email(content.get(1))
+                    .build();
+        }
+        catch (IOException e) {
+            return null;
+        }
+    }
+
 
 }
